e778d2cba6255891a3663afd7c0a8dab6cb5f457
Mounika Battu <mounika.battu@Mounikas-MacBook-Pro.local>
2026-01-12 21:01:59 +0530

    Feat: Adding terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,64 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+# Artifact Registry
+resource "google_artifact_registry_repository" "docker_repo" {
+  location      = var.region
+  repository_id = "aspnet-repo"
+  format        = "DOCKER"
+}
+
+# Cloud Run Service Account
+resource "google_service_account" "cloudrun_sa" {
+  account_id   = "cloudrun-sa"
+  display_name = "Cloud Run Service Account"
+}
+
+# Allow Cloud Run to pull images
+resource "google_project_iam_member" "artifact_access" {
+  role   = "roles/artifactregistry.reader"
+  member = "serviceAccount:${google_service_account.cloudrun_sa.email}"
+}
+
+# Cloud Run Service
+resource "google_cloud_run_service" "app" {
+  name     = "shoppingdemo-app"
+  location = var.region
+
+  template {
+    spec {
+      service_account_name = google_service_account.cloudrun_sa.email
+
+      containers {
+        image = "us-docker.pkg.dev/${var.project_id}/aspnet-repo/app:latest"
+        ports {
+          container_port = 8080
+        }
+      }
+    }
+  }
+
+  traffics {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+# Public access
+resource "google_cloud_run_service_iam_member" "public" {
+  service  = google_cloud_run_service.app.name
+  location = google_cloud_run_service.app.location
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+}
