793eefb3ab55e1f9c850f0f51a20434ba546a722
Mounika Battu <mounika.battu@Mounikas-MacBook-Pro.local>
2026-01-13 14:51:52 +0530

    Feat: Adding terraform files

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,8 +84,10 @@ data "google_iam_policy" "noauth" {
   }
 }
 
-resource "google_cloud_run_service_iam_policy" "default" {
-  location    = google_cloud_run_v2_service.default.location
-  service     = google_cloud_run_v2_service.default.name
-  policy_data = data.google_iam_policy.noauth.policy_data
+resource "google_cloud_run_v2_service_iam_member" "public" {
+  location = google_cloud_run_v2_service.default.location
+  project  = google_cloud_run_v2_service.default.project
+  service  = google_cloud_run_v2_service.default.name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
 }
